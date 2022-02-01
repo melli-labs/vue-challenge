e7fa9fffa1b06df632c0cfc56676e206db63fbdf
Felix Andreas <felix.andreas95@googlemail.com>
2022-02-01 19:21:22 +0100

    feat: add search to task 5

--- src/pages/task5/index.vue
+++ src/pages/task5/index.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
+import { useFuse } from '@vueuse/integrations/useFuse'
 import type { Note } from '~/types'
 
-const mockData: Array<Note> = [
+const mockData: Note[] = [
   {
     title: 'One more thing',
     body: 'The readonly notes cannot be edited or deleted. All other notes should be editable or deletable via the dropdown on the top right.',
@@ -30,28 +31,45 @@ const mockData: Array<Note> = [
     updatedAt: '2022-01-31 16:52:20',
   },
 ]
+
+const input = ref('')
+const { results } = useFuse(input, mockData, {
+  fuseOptions: { keys: ['title', 'body', 'author'] },
+  matchAllWhenSearchEmpty: true,
+})
 </script>
 
 <template>
   <div class="grid gap-6">
-    <div class="flex gap-4">
-      <router-link to="/task5/new" class="bg-primary-100 text-primary-800 font-medium rounded-md flex border-2 border-primary-200 h-12 shadow-sm px-3 gap-1.5 items-center focus-within:outline-none focus-within:border-primary-500 focus-within:ring-3 focus-within:ring-primary-300">
-        <div class="i-heroicons-outline:plus" />
-        New Note
+    <div class="flex gap-4 flex-col sm:flex-row">
+      <router-link
+        to="/task5/new"
+        class="bg-primary-100 text-primary-800 font-medium rounded-md border-2 border-primary-200 h-12 shadow-sm px-3 flex gap-1.5 items-center focus:outline-none focus:border-primary-500 focus:ring-3 focus:ring-primary-300"
+      >
+        <div class="i-heroicons-outline:plus" />New Note
       </router-link>
       <div
         class="flex-grow bg-white rounded-md flex border-2 border-primary-200 h-12 shadow-sm px-3 gap-3 items-center focus-within:outline-none focus-within:border-primary-500 focus-within:ring-3 focus-within:ring-primary-300"
       >
         <div class="h-6 text-primary-700 w-6 i-heroicons-outline:search" />
-        <input autofocus class="flex-grow h-full fill-primary-700 focus:outline-none">
+        <input
+          v-model="input"
+          class="flex-grow h-full fill-primary-700 focus:outline-none"
+          autofocus
+        >
       </div>
-      <router-link to="/end" class="bg-tertiary-100 text-tertiary-800 font-medium rounded-md flex border-2 border-tertiary-200 h-12 shadow-sm px-3 gap-1.5 items-center focus-within:outline-none focus-within:border-primary-500 focus-within:ring-3 focus-within:ring-primary-300">
-        <div class="i-heroicons-outline:chevron-right" />
-        Finish task
+      <router-link
+        to="/end"
+        class="bg-tertiary-100 text-tertiary-800 font-medium rounded-md border-2 border-tertiary-200 h-12 shadow-sm px-3 flex gap-1.5 items-center focus:outline-none focus:border-primary-500 focus:ring-3 focus:ring-primary-300"
+      >
+        <div class="i-heroicons-outline:chevron-right" />Finish task
       </router-link>
     </div>
-    <div class="grid gap-4 md:min-w-screen-sm lg:grid-cols-2">
-      <Note v-for="note in mockData" :key="note.key" :note="note" />
+    <div v-if="results.length > 0" class="grid gap-4 md:min-w-screen-sm lg:grid-cols-2">
+      <Note v-for="(result, index) in results" :key="index" :note="result.item" />
+    </div>
+    <div v-else class="min-h-xs grid place-items-center text-2xl text-primary-700 font-medium tracking-wide">
+      No results
     </div>
   </div>
 </template>
