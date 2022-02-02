d3b69b04a557d458a9bd86de4b27c2fb151fc3c3
Felix Andreas <felix.andreas95@googlemail.com>
2022-02-02 16:46:22 +0100

    chore: release v1
    Co-authored-by: Hans Ganter <hans.ganter@meetap.de>

--- README.md
+++ README.md
@@ -1,24 +1,18 @@
 <h2 align="center">
-🏗️ Under construction ...
+🛑 No open frontend positions at the moment 🛑
 </h2>
 
-# Emilia Vue Challenge
+# Emilia Vue 🖖 Challenge
 
 Hi 👋, I am Emilia!
 
-## Tech Stack
+Can you solve my frontend challenge?
 
-> Based on [Vitesse template](https://github.com/antfu/vitesse-lite)
-- ⚡️ [Vue 3](https://github.com/vuejs/vue-next), [Vite 2](https://github.com/vitejs/vite), [pnpm](https://pnpm.js.org/)
-- [🧭 Vue Router](https://github.com/vuejs/vue-router)
-  - [🗂 `vite-plugin-pages`](https://github.com/hannoeru/vite-plugin-pages) - file system based routing
-- 🎨 [UnoCSS](https://github.com/antfu/unocss) (On-demand [TailwindCSS](https://tailwindcss.com/)) - The instant on-demand atomic CSS engine.
-    - 😃 Use icons from any icon sets in [Pure CSS](https://github.com/antfu/unocss/tree/main/packages/preset-icons)
-- [🧩 VueUse](https://github.com/antfu/vueuse) - collection of useful composition APIs
-- [📥 `unplugin-auto-import`](https://github.com/antfu/unplugin-auto-import) - Directly use Vue Composition API and others without importing
-- [📦 `unplugin-vue-components`](https://github.com/antfu/unplugin-vue-components) - components auto import
-- 🔥 Use the [new `<script setup>` style](https://staging.vuejs.org/api/sfc-script-setup.html)
+## Challenge
 
+This repo is a simple Vue 3 project with four different routes. Each route contains a different task focusing on Vue 3's Composition API, TailwindCSS & UI design, and the usage of a simple REST API. 
+
+The goal is to solve all tasks and create a pull request to this repository with your solution.
 
 ## Setup
 
@@ -36,3 +30,17 @@ pnpm dev
 
 When you finish all tasks create a pull request. We would love to interview you 😍!
 
+
+## Tech Stack
+
+> Based on [Vitesse template](https://github.com/antfu/vitesse-lite)
+
+- ⚡️ [Vue 3](https://github.com/vuejs/vue-next), [Vite 2](https://github.com/vitejs/vite), [pnpm](https://pnpm.js.org/)
+- [🧭 Vue Router](https://github.com/vuejs/vue-router)
+  - [🗂 `vite-plugin-pages`](https://github.com/hannoeru/vite-plugin-pages) - file system based routing
+- 🎨 [UnoCSS](https://github.com/antfu/unocss) (On-demand [TailwindCSS](https://tailwindcss.com/)) - The instant on-demand atomic CSS engine.
+    - 😃 Use icons from any icon sets in [Pure CSS](https://github.com/antfu/unocss/tree/main/packages/preset-icons)
+- [🧩 VueUse](https://github.com/antfu/vueuse) - collection of useful composition APIs
+- [📥 `unplugin-auto-import`](https://github.com/antfu/unplugin-auto-import) - Directly use Vue Composition API and others without importing
+- [📦 `unplugin-vue-components`](https://github.com/antfu/unplugin-vue-components) - components auto import
+- 🔥 Use the [new `<script setup>` style](https://staging.vuejs.org/api/sfc-script-setup.html)
--- src/components/ui/Emilia.vue
+++ src/components/ui/Emilia.vue
@@ -19,7 +19,7 @@ const messages: Record<string, Message> = {
   },
   '/task3': {
     title: 'Task 3 - Interaction with a REST API',
-    description: 'In our third task, we will interact with a REST API 📥️. You can find the Swagger documentation at https://emilia-vue-challenge.deta.dev/docs. The goal is to implement a simple notes 📝 application - very creative I know 🤪. Right now, the notes you can see here are using mock data. To complete this last task, you need to fetch the real data from our database 📊. Furthermore, it should be possible to create a new note using the \'New Note\' button and edit and delete notes using the dropdown menu. Most of the user interface should already be implemented and you should only need to implement the API logic. But of course, you are free to make improvements to the current UI 😊. When you\'re done ✅, hit the big yellow button at the top right!',
+    description: 'In our third task, we will interact with a REST API 📥️. You can find the Swagger documentation at https://emilia-vue-challenge.deta.dev/docs. The goal is to implement a simple notes 📝 application. Right now, the notes you can see here are using mock data. To complete this last task, you need to fetch the real data from our database 📊. Furthermore, it should be possible to create a new note using the \'New Note\' button and edit and delete notes using the dropdown menu. Most of the user interface is already there and you only need to implement the API logic. But of course, you are free to make improvements to the current UI 😊. When you\'re done ✅, hit the big yellow button at the top right!',
   },
   '/task3/new': {
     title: 'Task 3 - New Note',
@@ -27,15 +27,15 @@ const messages: Record<string, Message> = {
   },
   '/task4': {
     title: 'Task 4 - Design Challenge 🎨',
-    description: 'In this last task, your creativity is needed! The goal is to design this contacts view. We have used our team as sample data so you can already get to know us 🤗. The first screen consists of a navbar and a list of contacts 👥. Clicking on a contact opens a more detailed view of a single contact 👤. I recommend starting with the styling of the navbar located in `components/NavBar.vue`. Then I would style the contacts-list in `components/ContactList.vue`. Finally, I would design the detail page in `components/ContactDetail.vue`. When you are satisfied with your solution, click on the check icon ✅ in the navbar to finish the challenge 🏁.',
+    description: 'In this last task, your creativity is needed! The goal is to design this contacts view. We used our team as sample data so you already get to know us 🤗. The first screen consists of a navbar and a list of contacts 👥. Clicking on a contact opens a more detailed view of a single contact 👤. I recommend starting with the styling of the navbar located in `components/NavBar.vue`. Then I would style the contacts-list in `components/ContactList.vue`. Finally, I would design the detail page in `components/ContactDetail.vue`. When you are satisfied with your solution, click on the check icon ✅ in the navbar to finish the challenge 🏁.',
   },
   '/task5': {
     title: 'Task 5 - Vue Router 🧭 & Pinia 🍍',
     description: 'under construction',
   },
   '/end': {
-    title: 'Wow 🔥',
-    description: 'Amazing 🤩! This is really impressive. We would love to get in touch with you 💯. Therefore, create a pull request 📥️ with your solution ✅ at https://github.com/mit-emilia/vue-challenge!',
+    title: 'You solved the challenge 🔥',
+    description: 'Amazing 🤩! This is impressive. We would love to get in touch with you 💯. Therefore, create a pull request 📥️ with your solution ✅ at https://github.com/mit-emilia/vue-challenge!',
   },
 }
 
