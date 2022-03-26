193a35edfafc1c485c81dd72de82f8698bf99c48
Your Name <fraeulein_k@gmx.net>
2022-03-26 14:59:34 +0100

    task 2 solved

--- src/components/Artwork.vue
+++ src/components/Artwork.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="grid border-4">
+  <div class="grid grid-rows-4 grid-cols-4 gap-1 border-4 p-1">
+    <div class="row-span-3 border-6 border-secondary-400" />
+    <div class="border-12 border-primary-400 rotate-45 scale-50" />
     <div class="border-6 border-primary-400" />
-    <div class="border-6 border-secondary-400" />
-    <div class="border-6 border-tertiary-400" />
     <div class="text-2xl font-semibold grid place-items-center">
       Emilia
     </div>
+    <div class="border-6 border-primary-400 " />
+    <div class="border-12 border-primary-400 rotate-45 scale-50 bg-primary-400" />
+    <div class="border-6 border-primary-400" />
+    <div class="border-6 border-primary-400 bg-primary-400" />
+    <div class="border-6 border-primary-400" />
+    <div class="border-12 border-primary-400 rotate-45 scale-50" />
+    <div class="text-2xl font-semibold grid place-items-center">
+      Emilia
+    </div>
+    <div class="col-span-3 border-6 border-tertiary-400" />
     <!-- you probably need a few more of these divs ... -->
   </div>
 </template>
