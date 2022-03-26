b02d3c4eb6f7014b466bb5842ce60fc1a3c7cd60
Your Name <fraeulein_k@gmx.net>
2022-03-26 15:01:06 +0100

    implemented get, fixed key

--- src/pages/task3/index.vue
+++ src/pages/task3/index.vue
@@ -1,40 +1,44 @@
 <script setup lang="ts">
 import { useFuse } from '@vueuse/integrations/useFuse'
+import { useFetch } from '@vueuse/core'
 import type { Note } from '~/types'
 
-const mockData: Note[] = [
-  {
-    title: 'One more thing',
-    body: 'There are some notes which are readonly 🔒️. These notes cannot be edited or deleted. But for the other notes we should be able to use the dropdown to edit ✍️ or delete ❌ them.',
-    author: 'Emilia',
-    key: 'f24jv9ss',
-    readonly: true,
-    createdAt: '2022-01-31 09:01:33',
-    updatedAt: '2022-01-31 09:01:33',
-  },
-  {
-    title: 'Tip: useFetch from VueUse 🧩',
-    body: 'Consider using the useFetch utility from the VueUse package for this task. It provides a convenient and reactive wrapper for the Fetch API. But of course you are free to install any other fetching library 📥️ or just use the Fetch API directly.',
-    author: 'Felix',
-    key: 'j3hhw92j',
-    readonly: true,
-    createdAt: '2022-01-24 12:12:45',
-    updatedAt: '2022-01-24 12:12:45',
-  },
-  {
-    title: '📝 TODO',
-    body: 'Hire a frontend developer.',
-    author: 'Hans',
-    readonly: false,
-    key: 'jfpnzy2nu',
-    createdAt: '2022-01-27 16:52:20',
-    updatedAt: '2022-01-31 16:52:20',
-  },
-]
+const { data } = await useFetch('https://emilia-vue-challenge.deta.dev/notes').get().json()
+const notes: Note[] = [...data.value]
+
+// const mockData: Note[] = [
+//   {
+//     title: 'One more thing',
+//     body: 'There are some notes which are readonly 🔒️. These notes cannot be edited or deleted. But for the other notes we should be able to use the dropdown to edit ✍️ or delete ❌ them.',
+//     author: 'Emilia',
+//     key: 'f24jv9ss',
+//     readonly: true,
+//     createdAt: '2022-01-31 09:01:33',
+//     updatedAt: '2022-01-31 09:01:33',
+//   },
+//   {
+//     title: 'Tip: useFetch from VueUse 🧩',
+//     body: 'Consider using the useFetch utility from the VueUse package for this task. It provides a convenient and reactive wrapper for the Fetch API. But of course you are free to install any other fetching library 📥️ or just use the Fetch API directly.',
+//     author: 'Felix',
+//     key: 'j3hhw92j',
+//     readonly: true,
+//     createdAt: '2022-01-24 12:12:45',
+//     updatedAt: '2022-01-24 12:12:45',
+//   },
+//   {
+//     title: '📝 TODO',
+//     body: 'Hire a frontend developer.',
+//     author: 'Hans',
+//     readonly: false,
+//     key: 'jfpnzy2nu',
+//     createdAt: '2022-01-27 16:52:20',
+//     updatedAt: '2022-01-31 16:52:20',
+//   },
+// ]
 
 const isFetching = false
 const input = ref('')
-const { results } = useFuse(input, mockData, {
+const { results } = useFuse(input, notes, {
   fuseOptions: { keys: ['title', 'body', 'author'] },
   matchAllWhenSearchEmpty: true,
 })
@@ -70,7 +74,7 @@ const { results } = useFuse(input, mockData, {
       <div class="i-tabler:loader-quarter animate-spin w-6 h-6 text-primary-800" />
     </div>
     <div v-else-if="results.length > 0" class="grid gap-4 md:min-w-screen-sm lg:grid-cols-2">
-      <Note v-for="(result, index) in results" :key="index" :note="result.item" />
+      <Note v-for="(result) in results" :key="result.item.key" :note="result.item" />
     </div>
     <div v-else class="min-h-xs grid place-items-center text-2xl text-primary-700 font-medium tracking-wide">
       No results
