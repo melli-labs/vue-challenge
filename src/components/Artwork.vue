6fb6d54f2413f14f085782b1440c4ba2669ce955
David Parsons <info@dpdesigns.eu>
2022-03-29 21:50:00 +0200

    task 2 completed

--- src/components/Artwork.vue
+++ src/components/Artwork.vue
@@ -1,11 +1,30 @@
 <template>
-  <div class="grid border-4">
-    <div class="border-6 border-primary-400" />
-    <div class="border-6 border-secondary-400" />
-    <div class="border-6 border-tertiary-400" />
-    <div class="text-2xl font-semibold grid place-items-center">
+  <div class="grid border-4 grid-cols-4 grid-rows-4 p-1 gap-1">
+    <div class="border-6 border-secondary-400 col-span-1 row-span-3" />
+    <div
+      class="border-12 border-primary-400 col-span-1 row-span-1 rotate-45 scale-50"
+    />
+    <div class="border-6 border-primary-400 col-span-1 row-span-1" />
+    <div
+      class="text-2xl font-semibold grid col-span-1 row-span-1 place-items-center"
+    >
       Emilia
     </div>
-    <!-- you probably need a few more of these divs ... -->
+    <div class="border-6 border-primary-400 col-span-1 row-span-1" />
+    <div
+      class="border-12 border-primary-400 rotate-45 scale-50 bg-primary col-span-1 row-span-1"
+    />
+    <div class="border-6 border-primary-400 col-span-1 row-span-1" />
+    <div class="border-6 border-primary-400 bg-primary col-span-1 row-span-1" />
+    <div class="border-6 border-primary-400 col-span-1 row-span-1" />
+    <div
+      class="border-12 border-primary-400 col-span-1 row-span-1 rotate-45 scale-50"
+    />
+    <div
+      class="text-2xl font-semibold grid place-items-center col-span-1 row-span-1"
+    >
+      Emilia
+    </div>
+    <div class="border-6 border-tertiary-400 col-span-3 row-span-1" />
   </div>
 </template>
