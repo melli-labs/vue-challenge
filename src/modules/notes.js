593610fae32e4e1075752c9dba91736f0c7ca091
David Parsons <info@dpdesigns.eu>
2022-04-03 21:06:34 +0200

    some issues with refreshing data - need to read more on how ref and computed work with script setup

--- src/modules/notes.js
+++ src/modules/notes.js
@@ -5,11 +5,11 @@ export default function useNotes() {
   const error = ref(null);
   const notes = ref([]);
   // const useNotes() {
-  //   // super complicated cart logic
   //   return {
   //     notes: computed(() => notes.value),
   //   };
   // }
+
   const isFetching = ref(true);
   const baseUrl = ref(`https://emilia-vue-challenge.deta.dev/notes`);
   const refetch = { refetch: true };
