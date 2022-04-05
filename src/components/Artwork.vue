4777e774c0840c209389701a7e94185c5571c741
Mohammad Najafi <100156282+MHNK1985@users.noreply.github.com>
2022-04-05 18:42:15 +0430

    Update Artwork.vue

--- src/components/Artwork.vue
+++ src/components/Artwork.vue
@@ -1,11 +1,56 @@
 <template>
-  <div class="grid border-4">
-    <div class="border-6 border-primary-400" />
-    <div class="border-6 border-secondary-400" />
-    <div class="border-6 border-tertiary-400" />
-    <div class="text-2xl font-semibold grid place-items-center">
-      Emilia
+  <div class="grid grid-cols-4 grid-rows-4 border-4 gap-1 p-1">
+    <div class="border-6 border-secondary-400 row-span-3" />
+    <div
+      class="
+        grid
+        justify-items-stretch
+        grid-rows-3 grid-cols-3
+        gap-1
+        col-span-3
+        row-span-3
+      "
+    >
+      <div
+        class="
+          border-6 border-primary-400
+          justify-self-center
+          self-center
+          w-6/12
+          h-6/12
+          rotate-45
+        "
+      />
+      <div class="border-6 border-primary-400" />
+      <div class="text-2xl font-semibold grid place-items-center">Emilia</div>
+      <div class="border-6 border-primary-400" />
+      <div
+        class="
+          border-6 border-primary-400
+          bg-primary-400
+          justify-self-center
+          self-center
+          w-6/12
+          h-6/12
+          rotate-45
+        "
+      />
+      <div class="border-6 border-primary-400" />
+      <div class="border-6 border-primary-400 bg-primary-400" />
+      <div class="border-6 border-primary-400" />
+      <div
+        class="
+          border-6 border-primary-400
+          justify-self-center
+          self-center
+          w-6/12
+          h-6/12
+          rotate-45
+        "
+      />
     </div>
+    <div class="text-2xl font-semibold grid place-items-center">Emilia</div>
+    <div class="border-6 border-tertiary-400 col-span-3" />
     <!-- you probably need a few more of these divs ... -->
   </div>
 </template>
