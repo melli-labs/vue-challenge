32d056eb4640693537080245bf117d8a9dd409c1
Mohammad Najafi <100156282+MHNK1985@users.noreply.github.com>
2022-04-05 18:47:09 +0430

    Update Note.vue

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,43 +1,108 @@
 <script setup lang="ts">
-import type { Note } from '~/types'
+import type { Note } from "~/types";
 
 const props = defineProps<{
-  note: Note
-}>()
+  note: Note;
+}>();
 
-const dropdown = ref(false)
-const dropdownRef = ref(null)
+const dropdown = ref(false);
+const dropdownRef = ref(null);
 onClickOutside(dropdownRef, (event) => {
-  event.stopPropagation()
-  dropdown.value = false
-})
+  event.stopPropagation();
+  dropdown.value = false;
+});
 
-const edit = ref(false)
-const title = ref(props.note.title)
-const body = ref(props.note.body)
-const isUpdating = false
+//const edit = ref(false)
+//const title = ref(props.note.title)
+//const body = ref(props.note.body)
+//const isUpdating = false
 
-const deleteNote = () => alert('not implemented')
-const updateNote = () => alert('not implemented')
+//const deleteNote = () => alert('not implemented')
+
+import axios from "axios";
+const emiliaApi = axios.create({});
+const noteid = ref(props.note.key);
+
+const deleteNote = () => {
+  emiliaApi
+    .delete(`https://emilia-vue-challenge.deta.dev/notes/${noteid.value}`)
+    .then(() => {
+      alert("delete done");
+    })
+    .catch(() => {});
+};
+console.log(deleteNote);
+
+//const updateNote = () => alert('not implemented')
+const isUpdating = ref(false);
+const updateNote = () => {
+  isUpdating.value = true;
+  emiliaApi
+    .put(`https://emilia-vue-challenge.deta.dev/notes/${noteid.value}`, {
+    })
+    .then(() => {
+      alert("edite done");
+    })
+    .catch(() => {});
+};
+console.log(updateNote);
 </script>
 
 <template>
-  <div class="rounded-md bg-white border-2 border-primary-200 shadow-sm overflow-hidden">
-    <header class="px-3 py-2 flex bg-primary-100 gap-2 items-center text-sm text-primary-800">
+  <div
+    class="
+      rounded-md
+      bg-white
+      border-2 border-primary-200
+      shadow-sm
+      overflow-hidden
+    "
+  >
+    <header
+      class="
+        px-3
+        py-2
+        flex
+        bg-primary-100
+        gap-2
+        items-center
+        text-sm text-primary-800
+      "
+    >
       <div
-        class="h-10 w-10 uppercase rounded-full grid place-items-center font-bold bg-primary-50"
+        class="
+          h-10
+          w-10
+          uppercase
+          rounded-full
+          grid
+          place-items-center
+          font-bold
+          bg-primary-50
+        "
       >
         {{ note.author.slice(0, 2) }}
       </div>
       <div>
         <span>@{{ note.author }}</span>
-        <span class="opacity-65">{{ ' commented on ' }}</span>
+        <span class="opacity-65">{{ " commented on " }}</span>
         <span>{{ new Date(note.createdAt).toDateString() }}</span>
-        <span v-if="note.updatedAt !== note.createdAt">· edited</span>
+        <span v-if="note.updatedAt !== note.createdAt">آ· edited</span>
       </div>
       <div
         v-if="note.readonly"
-        class="ml-auto border-2 rounded-full flex gap-1 bg-primary-50 border-primary-200 px-2 py-1 items-center"
+        class="
+          ml-auto
+          border-2
+          rounded-full
+          flex
+          gap-1
+          bg-primary-50
+          border-primary-200
+          px-2
+          py-1
+          items-center
+        "
       >
         <div class="i-heroicons-outline:lock-closed opacity-65" />
         <span class="text-xs">readonly</span>
@@ -54,18 +119,47 @@ const updateNote = () => alert('not implemented')
           <div
             v-if="dropdown"
             ref="dropdownRef"
-            class="dropdown-menu transition duration-200 absolute text-base py-2 -right-2 bg-white border border-gray-200 grid rounded-md shadow"
+            class="
+              dropdown-menu
+              transition
+              duration-200
+              absolute
+              text-base
+              py-2
+              -right-2
+              bg-white
+              border border-gray-200
+              grid
+              rounded-md
+              shadow
+            "
             @click="dropdown = false"
           >
             <button
-              class="px-6 py-2 text-gray-800 hover:bg-gray-50 flex items-center gap-1"
+              class="
+                px-6
+                py-2
+                text-gray-800
+                hover:bg-gray-50
+                flex
+                items-center
+                gap-1
+              "
               @click="edit = true"
             >
               <span class="i-heroicons-outline:pencil-alt w-6 h-6 opacity-65" />
               Edit
             </button>
             <button
-              class="px-6 py-2 text-danger-600 hover:bg-danger-50 flex items-center gap-1"
+              class="
+                px-6
+                py-2
+                text-danger-600
+                hover:bg-danger-50
+                flex
+                items-center
+                gap-1
+              "
               @click="deleteNote"
             >
               <span class="i-heroicons-outline:x w-6 h-6 opacity-65" />
@@ -76,24 +170,62 @@ const updateNote = () => alert('not implemented')
       </div>
     </header>
     <main class="p-6 text-gray-800 grid gap-2">
-      <input v-if="edit" v-model="title" class="input">
+      <input v-if="edit" v-model="title" class="input" />
       <h1 v-else class="font-medium text-lg">
         {{ note.title }}
       </h1>
-      <textarea v-if="edit" v-model="body" name="body" rows="3" class="min-h-40 p-4 input" />
+      <textarea
+        v-if="edit"
+        v-model="body"
+        name="body"
+        rows="3"
+        class="min-h-40 p-4 input"
+      />
       <p v-else>
         {{ note.body }}
       </p>
       <div v-if="edit" class="flex flex-row-reverse">
         <button
-          class="bg-primary-50 font-medium rounded-md flex border-2 h-12 shadow-sm px-3 gap-1.5 items-center focus:outline-none focus:border-primary-500 focus:ring-3 focus:ring-primary-300"
-          :class="isUpdating ? 'text-primary-500 border-primary-100' : 'text-primary-800  border-primary-200'"
+          class="
+            bg-primary-50
+            font-medium
+            rounded-md
+            flex
+            border-2
+            h-12
+            shadow-sm
+            px-3
+            gap-1.5
+            items-center
+            focus:outline-none
+            focus:border-primary-500
+            focus:ring-3
+            focus:ring-primary-300
+          "
+          :class="
+            isUpdating
+              ? 'text-primary-500 border-primary-100'
+              : 'text-primary-800  border-primary-200'
+          "
           @click="updateNote"
         >
-          {{ isUpdating ? 'Updating Note ...': 'Update Note' }}
+          {{ isUpdating ? "Updating Note ..." : "Update Note" }}
         </button>
         <button
-          class="text-primary-800 font-medium rounded-md flex h-12 px-3 gap-1.5 items-center focus:outline-none focus:border-primary-500 focus:ring-3 focus:ring-primary-300"
+          class="
+            text-primary-800
+            font-medium
+            rounded-md
+            flex
+            h-12
+            px-3
+            gap-1.5
+            items-center
+            focus:outline-none
+            focus:border-primary-500
+            focus:ring-3
+            focus:ring-primary-300
+          "
           @click="edit = false"
         >
           Cancel
