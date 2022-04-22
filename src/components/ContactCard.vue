848c9cbdeb061d471a8d04e3a8b4d80b5c01895a
Jack <jackmayer21@gmail.com>
2022-04-21 20:17:39 -0500

    get,del,put todos, style contacts

---
+++ src/components/ContactCard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { Person } from "~/types"
+
+defineProps<{ person: Person }>()
+</script>
+
+<template>
+  <div
+    class="card ma-3 pt-0.5 bg-white hover:bg-primary items-center text-center rounded-lg"
+  >
+    <img
+      :src="`https://meetap-it.gitlab.io/emilia/assets/team/${person.handle}.webp`"
+      class="w-12 h-12 rounded-full ma-auto mt-4"
+    >
+    <div>{{ person.name }}</div>
+    <div>{{ person.role }}</div>
+  </div>
+</template>
+<style>
+.card {
+  background: linear-gradient(
+    to right,
+    rgb(209, 250, 229) 0,
+    rgba(255, 255, 255, 0.7) 50%,
+    rgb(209, 250, 229) 100%
+  );
+}
+</style>
