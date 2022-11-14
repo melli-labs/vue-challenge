32cf29a29d4330902f7ba545d7eee41f768a6777
Nico Franke <nico.franke01@gmail.com>
2022-11-14 17:56:37 +0100

    Second task

--- src/components/Artwork.vue
+++ src/components/Artwork.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="grid border-4">
+  <div class="grid grid-cols-4 grid-rows-4 gap-1 border-4 p-1">
+    <div class="border-6 border-secondary-400 row-span-3" />
+    <div class="border-6 border-primary-400 transform rotate-45 m-6" />
     <div class="border-6 border-primary-400" />
-    <div class="border-6 border-secondary-400" />
-    <div class="border-6 border-tertiary-400" />
-    <div class="text-2xl font-semibold grid place-items-center">
-      Melli
+    <div class="text-2xl grid place-items-center font-semibold">
+      Emilia
     </div>
-    <!-- you probably need a few more of these divs ... -->
+    <div class="border-6 border-primary-400" />
+    <div class="bg-primary-400 transform rotate-45 m-6" />
+    <div class="border-6 border-primary-400" />
+    <div class="bg-primary-400" />
+    <div class="border-6 border-primary-400" />
+    <div class="border-6 border-primary-400 transform rotate-45 m-6" />
+    <div class="text-2xl grid place-items-center font-semibold">
+      Emilia
+    </div>
+    <div class="border-6 border-tertiary-400 col-span-3" />
   </div>
 </template>
